bf0f5dc0a382c6e3353a3a51cadf022d59041c46
Paterne Tia <lingotacro@gmail.com>
2024-10-21 14:31:38 +0000

    initial commit

---
+++ app/ui/dashboard/nav-links.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import {
+  UserGroupIcon,
+  HomeIcon,
+  DocumentDuplicateIcon,
+} from '@heroicons/react/24/outline';
+import clsx from 'clsx';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+// Map of links to display in the side navigation.
+const links = [
+  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  {
+    name: 'Invoices',
+    href: '/dashboard/invoices',
+    icon: DocumentDuplicateIcon,
+  },
+  { name: 'Costumers', href: '/dashboard/costumers', icon: UserGroupIcon },
+];
+
+export default function NavLinks() {
+  // Make sure usePathname is being imported and used correctly.
+  const pathname = usePathname();
+
+  if (!pathname) {
+    return null; // Return null if pathname is not defined to avoid potential errors
+  }
+
+  return (
+    <>
+      {links.map((link) => {
+        const LinkIcon = link.icon;
+        const isActive = pathname === link.href;
+
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-blue-600': isActive,
+              }
+            )}
+          >
+            <LinkIcon className="w-6" />
+            <p className="hidden md:block">{link.name}</p>
+          </Link>
+        );
+      })}
+    </>
+  );
+}
